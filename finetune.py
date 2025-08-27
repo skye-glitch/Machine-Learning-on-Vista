3c14040011d67f99a091e1e63aef885028c79af4
skye-glitch <62414340+skye-glitch@users.noreply.github.com>
2025-08-27 11:29:38 -0500

    Create finetune.py

---
+++ finetune.py
@@ -0,0 +1,35 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+from transformers import TrainingArguments
+from transformers import Trainer
+
+
+
+raw_datasets = load_dataset("glue", "mrpc")
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+training_args = TrainingArguments("test-trainer")
+
+from transformers import AutoModelForSequenceClassification
+
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    processing_class=tokenizer,
+)
+
+trainer.train()
